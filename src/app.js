03da796f679266c34d4e624c60b0cc09f75cb36e
Ferchisqs <selihgamer6@gmail.com>
2025-02-03 14:48:17 -0600

    solucionando errores

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/auth.routes'); 
-const sequelize = require('./config/database');
-const User = require('./models/User');
+const routes = require('../src/routes/auth.routes'); 
+const discoRoutes = require('../src/routes/disco.routes');  // Importa las rutas de discos
+const sequelize = require('../src/config/database');
+
 
 const app = express();
 
-
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
+
+app.use('/api', discoRoutes); 
+
 sequelize.sync({ alter: true })
   .then(() => {
-    console.log("🔄 Tablas sincronizadas");
+    console.log("Tablas sincronizadas");
 
     app.listen(3000, '0.0.0.0', () => {
-      console.log('Servidor corriendo en http://0.0.0.0:3000');
+      console.log('Servidor corriendo en ');
     });
 
   })
--- src/routes/disco.routes.js
+++ src/routes/disco.routes.js
@@ -4,6 +4,7 @@ const { obtenerDiscos, agregarDisco } = require('../controllers/disco.controller
 const router = express.Router();
 
 router.get('/discos', obtenerDiscos);
+
 router.post('/discos', agregarDisco);
 
 module.exports = router;
